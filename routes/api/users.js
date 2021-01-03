c47e8fe577200eb3a957a50bae263701da829a3a
Abhay Rajendra Dixit <abhaydixit95@gmail.com>
2021-01-03 16:39:43 -0500

    Added create post functionality

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,7 +5,7 @@ const bcrypt = require("bcryptjs");
 const jwt = require('jsonwebtoken');
 const config = require('config');
 const { check, validationResult } = require("express-validator");
-
+const normalize = require('normalize-url');
 const User = require("../../models/User");
 
 // @route   POST api/users
@@ -39,11 +39,14 @@ router.post(
           .json({ errors: [{ msg: "User already exists" }] });
       }
 
-      const avatar = gravatar.url(email, {
-        s: "200",
-        r: "pg",
-        d: "mm",
-      });
+      const avatar = normalize(
+        gravatar.url(email, {
+          s: '200',
+          r: 'pg',
+          d: 'mm'
+        }),
+        { forceHttps: true }
+      );
 
       user = new User({
         name,
