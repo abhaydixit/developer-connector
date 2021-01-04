a89d30f8e98ccb711aa10627ed9cc9218dde94f1
Abhay Rajendra Dixit <abhaydixit95@gmail.com>
2021-01-04 15:17:54 -0500

    Added liked and unlike

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -6,6 +6,7 @@ const auth = require('../../middleware/auth');
 const Post = require('../../models/Post');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const { post } = require("./profile");
 
 
 // @route   POST api/posts
@@ -74,7 +75,7 @@ router.get('/:id', auth, async (req, res) => {
         if(!post){
             return res.status(404).json({msg: 'Post not found!'})
         }
-
+ 
         res.json(post);
     } catch (error) {
         console.log(error.message);
@@ -115,5 +116,58 @@ router.delete('/:id', auth, async (req, res) => {
         res.status(500).send('Server Error');
     }
 })
+
+
+// @route   PUT api/posts/like/:id
+// @desc    Like a post
+// @access  Private
+router.put('/like/:id', auth, async (req, res) => {
+    console.log("Here")
+
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if(post.likes.filter(like => like.user.toString() === req.user.id).length > 0){
+            return res.status(400).json({msg: 'Post already liked'});
+        }
+
+        post.likes.unshift({user: req.user.id});
+
+        await post.save();
+
+        res.json(post.likes);
+
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+
+
+router.put('/unlike/:id', auth, async (req, res) => {
+    console.log("Here")
+
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if(post.likes.filter(like => like.user.toString() === req.user.id).length === 0){
+            return res.status(400).json({msg: 'Post has not yet been liked'});
+        }
+
+        const removeIndex = post.likes.map(like => like.user.toString()).indexOf(req.user.id);
+
+        post.likes.splice(removeIndex, 1);
+        
+        await post.save();
+
+        res.json(post.likes);
+
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send('Server Error');
+    }
+});
+
 
 module.exports = router;
